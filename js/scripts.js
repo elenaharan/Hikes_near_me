9b0caed4d712bdae6565947b1113af0b6192fb48
Elena Haran <elenaharan@gmail.com>
2021-07-13 12:36:42 +0100

    I have created Pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = "pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}, 
+    {name: 'Ivtsaur', height: 1, type: ['grass', 'poison']}, 
+    {name: 'Venusaur', height: 2, type: ['grass', 'poison']}, 
+    {name: 'Charmander', height: 0.6, type: 'fire'}
+];
