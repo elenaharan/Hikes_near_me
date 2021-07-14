94408bdd4a6ef91a837e516b1194fe7aa42c3542
Elena Haran <elenaharan@gmail.com>
2021-07-14 19:02:23 +0100

    I added loops and conditionals to my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}, 
-    {name: 'Ivtsaur', height: 1, type: ['grass', 'poison']}, 
-    {name: 'Venusaur', height: 2, type: ['grass', 'poison']}, 
-    {name: 'Charmander', height: 0.6, type: 'fire'}
+    {
+    name: 'Bulbasaur', 
+    height: 0.7, 
+    type: ['grass', 'poison']
+}, 
+    {
+    name: 'Ivtsaur', 
+    height: 1, 
+    type: ['grass', 'poison']
+}, 
+    {
+    name: 'Venusaur', 
+    height: 2, 
+    type: ['grass', 'poison']
+}, 
+    {
+    name: 'Charmander', 
+    height: 0.6, 
+    type: 'fire'
+}
 ];
+//this loop simply prints name of every pokemon in the array//
+/*for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " ");
+}*/
+
+//This loop returns Pokemon's name and height//
+/* let text = "";
+let i = 0;
+while (pokemonList[i]) {
+    text = text + "  " + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + "),";
+    i++;
+}
+
+document.write(text);*/
+
+//This loop contains a conditional//
+let text = "";
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 0.5 && pokemonList[i].height < 2) {
+    document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
+    pokemonList[i].height + "),");
+  } else if (pokemonList[i].height >= 2) {
+    document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
+    pokemonList[i].height + ")" + " " + "-" + " " + "Wow, that's big!");
+}
+}
