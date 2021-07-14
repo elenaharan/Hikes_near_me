dc06fb9a395499e4e020c4152ed20f7f0a4424f0
Elena Haran <elenaharan@gmail.com>
2021-07-14 20:30:10 +0100

    Each item is now on a new line

--- js/scripts.js
+++ js/scripts.js
@@ -20,29 +20,32 @@ let pokemonList = [
     type: 'fire'
 }
 ];
+
 //this loop simply prints name of every pokemon in the array//
 /*for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " ");
+    document.write(pokemonList[i].name + " " + "<br>");
 }*/
 
 //This loop returns Pokemon's name and height//
-/* let text = "";
+/*
+ let text = "";
 let i = 0;
 while (pokemonList[i]) {
-    text = text + "  " + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + "),";
+    text = text + "  " + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")," + "<br>";
     i++;
 }
 
-document.write(text);*/
+document.write(text);
+*/
 
 //This loop contains a conditional//
 let text = "";
 for (let i=0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 0.5 && pokemonList[i].height < 2) {
     document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
-    pokemonList[i].height + "),");
+    pokemonList[i].height + ")," + "<br>");
   } else if (pokemonList[i].height >= 2) {
     document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
-    pokemonList[i].height + ")" + " " + "-" + " " + "Wow, that's big!");
+    pokemonList[i].height + ")" + " " + "-" + " " + "Wow, that's big!" + "<br>");
 }
 }
