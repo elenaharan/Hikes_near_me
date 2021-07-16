dbdc4f32a2c13c6dd7fb008bb0d414e005eb1a0a
Elena Haran <elenaharan@gmail.com>
2021-07-16 20:37:20 +0100

    I used forEach() function in place of the for loop

--- js/scripts.js
+++ js/scripts.js
@@ -21,31 +21,7 @@ let pokemonList = [
 }
 ];
 
-//this loop simply prints name of every pokemon in the array//
-/*for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " " + "<br>");
-}*/
-
-//This loop returns Pokemon's name and height//
-/*
- let text = "";
-let i = 0;
-while (pokemonList[i]) {
-    text = text + "  " + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")," + "<br>";
-    i++;
-}
-
-document.write(text);
-*/
-
-//This loop contains a conditional//
-let text = "";
-for (let i=0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.5 && pokemonList[i].height < 2) {
-    document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
-    pokemonList[i].height + ")," + "<br>");
-  } else if (pokemonList[i].height >= 2) {
-    document.write(text + "  " + pokemonList[i].name + " " + "(height: " + 
-    pokemonList[i].height + ")" + " " + "-" + " " + "Wow, that's big!" + "<br>");
-}
-}
+pokemonList.forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name + ', ' + 'height: ' 
+    + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+})
