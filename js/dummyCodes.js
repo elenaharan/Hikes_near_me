3f3671c54f9be71f1756c9e96c5dcbb11c85ec97
Elena Haran <elenaharan@gmail.com>
2021-07-17 13:06:53 +0100

    I added condition to getAll() function

--- js/dummyCodes.js
+++ js/dummyCodes.js
@@ -27,6 +27,23 @@ for (let i=0; i < pokemonList.length; i++) {
 
 
 pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height > 1.5) {
+    document.write('<p>' + pokemon.name + ', ' + 'height: ' 
+    + pokemon.height + ', ' + 'type: ' + pokemon.type + " - Wow! That's big!" + '</p>')
+  }
   document.write('<p>' + pokemon.name + ', ' + 'height: ' 
   + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
 })
+
+
+
+
+
+
+function add(pokemon) {
+  if (typeof pokemon === object) {
+    pokemonList.push(pokemon);
+  } else {
+    document.write("You've enetered incorrect type of data!");
+  }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,25 @@ return {
 }
 }) ();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
+/*pokemonRepository.getAll().forEach(function(pokemon) {
     document.write('<p>' + pokemon.name + ', ' + 'height: ' 
     + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+  })*/
+
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height > 1.5) {
+      document.write('<p>' + pokemon.name + ', ' + 'height: ' 
+      + pokemon.height + ', ' + 'type: ' + pokemon.type + " - Wow! That's big!" + '</p>')
+    } else {
+      document.write('<p>' + pokemon.name + ', ' + 'height: ' 
+      + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+    }
   })
+
+  function add(pokemon) {
+    if (typeof pokemon === object) {
+      pokemonList.push(pokemon);
+    } else {
+      document.write("You've enetered incorrect type of data!");
+    }
+  }
